251ec5b0d93370f840179a50fe1244adaad37066
AzharSy3d <azharuworld@gmail.com>
2021-07-24 23:03:24 +0530

    Initial Project Setup, Setup Routes closes #1

---
+++ src/main/java/com/azsyed/lcbrewarygateway/LcBrewaryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.azsyed.lcbrewarygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcBrewaryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcBrewaryGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azsyed/lcbrewarygateway/LcBrewaryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azsyed.lcbrewarygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcBrewaryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
