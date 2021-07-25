966e1ea876afd5648924d7f4d594164ae1ba87b5
AzharSy3d <azharuworld@gmail.com>
2021-07-26 00:18:15 +0530

    Add Eureka Service Discovery closes #3

--- src/main/java/com/azsyed/lcbrewarygateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/azsyed/lcbrewarygateway/config/LoadBalancedRoutesConfig.java
@@ -1,2 +1,25 @@
-package com.azsyed.lcbrewarygateway.config;public class LoadBalancedRoutesConfig {
+package com.azsyed.lcbrewarygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@Configuration
+public class LoadBalancedRoutesConfig {
+
+
+    @Bean
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beer/upc/*")
+                        .uri("lb://beer-service"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("lb://order-service"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("lb://inventory-service"))
+                .build();
+    }
 }
