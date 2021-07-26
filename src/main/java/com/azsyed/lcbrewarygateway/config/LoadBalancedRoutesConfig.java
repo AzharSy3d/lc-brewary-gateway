7f7138152a0599ca4a5b84f49bef734e760a66c3
AzharSy3d <azharuworld@gmail.com>
2021-07-26 19:52:26 +0530

    Initial Project Setup

--- src/main/java/com/azsyed/lcbrewarygateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/azsyed/lcbrewarygateway/config/LoadBalancedRoutesConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+
 @Profile("local-discovery")
 @Configuration
 public class LoadBalancedRoutesConfig {
-
 
     @Bean
     public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder){
@@ -19,7 +19,14 @@ public class LoadBalancedRoutesConfig {
                 .route(r -> r.path("/api/v1/customers/**")
                         .uri("lb://order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+                                .setFallbackUri("forward:/inventory-failover")
+                                .setRouteId("inv-failover")
+                        ))
                         .uri("lb://inventory-service"))
+                .route(r -> r.path("/inventory-failover/**")
+                        .uri("lb://inventory-failover"))
                 .build();
     }
+
 }
